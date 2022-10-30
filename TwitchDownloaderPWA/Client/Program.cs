af93b06db074b89e9f877ace85052db396b4c665
PoofyFox <16710818+PoofImaFox@users.noreply.github.com>
2022-10-30 09:26:13 -0400

    Testing Changes

--- TwitchDownloaderPWA/Client/Program.cs
+++ TwitchDownloaderPWA/Client/Program.cs
@@ -1,17 +1,18 @@
+using BlazorStrap;
+
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
 using TwitchDownloaderPWA;
 
-namespace TwitchDownloaderPWA
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
+namespace TwitchDownloaderPWA {
+    public class Program {
+        public static async Task Main(string[] args) {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
+            builder.Services.AddBlazorStrap();
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
